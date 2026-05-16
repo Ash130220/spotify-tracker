39be17f51609e33794b34efbac2ecfef1c83a7a6
Ash130220 <anshkathrotiya94@gmail.com>
2026-05-16 22:41:51 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,78 @@
+const axios = require("axios");
+const fs = require("fs");
+
+const CLIENT_ID = "d2cea5ef680f41d28becac432f7b56dc";
+const CLIENT_SECRET = "f41e3c92b4ac4627b92052b42b05fdd3";
+
+// ONLY playlist ID
+const PLAYLIST_ID = "3RagErmb9JKqOCuizMm0in";
+
+async function getToken() {
+    const response = await axios.post(
+        "https://accounts.spotify.com/api/token",
+        new URLSearchParams({
+            grant_type: "client_credentials"
+        }),
+        {
+            headers: {
+                Authorization:
+                    "Basic " +
+                    Buffer.from(
+                        CLIENT_ID + ":" + CLIENT_SECRET
+                    ).toString("base64"),
+                "Content-Type": "application/x-www-form-urlencoded"
+            }
+        }
+    );
+
+    return response.data.access_token;
+}
+
+async function checkPlaylist() {
+    try {
+        const token = await getToken();
+
+        const response = await axios.get(
+            `https://api.spotify.com/v1/playlists/${PLAYLIST_ID}`,
+            {
+                headers: {
+                    Authorization: `Bearer ${token}`
+                }
+            }
+        );
+
+        const newSnapshot = response.data.snapshot_id;
+
+        let oldSnapshot = "";
+
+        if (fs.existsSync("data.json")) {
+            const data = JSON.parse(
+                fs.readFileSync("data.json", "utf8")
+            );
+
+            oldSnapshot = data.snapshot || "";
+        }
+
+        // print ONLY if changed
+        if (oldSnapshot && oldSnapshot !== newSnapshot) {
+            console.log("🔥 PLAYLIST UPDATED!");
+        }
+
+        // save snapshot
+        fs.writeFileSync(
+            "data.json",
+            JSON.stringify(
+                { snapshot: newSnapshot },
+                null,
+                2
+            )
+        );
+
+    } catch (err) {
+        console.log(err.response?.data || err.message);
+    }
+}
+
+checkPlaylist();
+
+setInterval(checkPlaylist, 5000);
